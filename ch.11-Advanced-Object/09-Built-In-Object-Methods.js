4a8b73ce4fbaa639ebdc0968002610798f36710d
MashoodAli-Official <MashoodAli3@gmail.com>
2025-02-02 20:23:25 +0400

    Add additional built-in object methods: introduce Object.seal(), Object.retreat(), Object.preventExtensions(), and Object.create() with examples

--- ch.11-Advanced-Object/09-Built-In-Object-Methods.js
+++ ch.11-Advanced-Object/09-Built-In-Object-Methods.js
@@ -35,3 +35,41 @@ const studentInfo3 = {
 Object.freeze(studentInfo3);
 studentInfo3.name = "Hermione Granger";
 console.log(studentInfo3);
+
+// 6. Object.seal() method helps to prevent new properties from being added to the object
+const studentInfo4 = {
+    name: "Harry Potter",
+    age: 16,
+    school: "Hogwarts",
+}
+Object.seal(studentInfo4);
+studentInfo4.name = "Hermione Granger";
+console.log(studentInfo4);
+
+// 7. Object.retreat() method helps to remove a property from the object
+const studentInfo5 = {
+    name: "Harry Potter",
+    age: 16,
+    school: "Hogwarts",
+}
+delete studentInfo5.name;
+console.log(studentInfo5);
+
+// 8. Object.preventExtensions() method helps to prevent the addition of new properties to the object
+const studentInfo6 = {
+    name: "Harry Potter",
+    age: 16,
+    school: "Hogwarts",
+}
+Object.preventExtensions(studentInfo6);
+studentInfo6.name = "Hermione Granger";
+console.log(studentInfo6);
+
+// 9. Object.create() method helps to create a new object
+const studentInfo7 = {
+    name: "Harry Potter",
+    age: 16,
+    school: "Hogwarts",
+}
+const studentInfo8 = Object.create(studentInfo7);
+console.log(studentInfo8);
